9758e19943a552c8d26a2b0ebb94d32a0d3d8eb1
夏目大人 <924787867@qq.com>
2020-07-29 15:15:06 +0800

    把本地工程添加到git

---
+++ src/com/tanzhishuju/FirstClass.java
@@ -0,0 +1,11 @@
+package com.tanzhishuju;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("something");
+        System.out.println("something");
+        System.out.println("something");
+        System.out.println("something");
+        System.out.println("something");
+    }
+}
